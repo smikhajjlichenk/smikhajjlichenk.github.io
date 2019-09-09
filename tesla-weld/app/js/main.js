0aee0bc41d19d619b9c58c9efb2ab3b9b86ee156
Andrew <48488675+smikhajjlichenk@users.noreply.github.com>
2019-09-09 22:38:34 +0300

    Add files via upload

---
+++ tesla-weld/app/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+	
+$("#timerdd79587a0c12b5aee748d795eabe2e86").appendTo(".header__main-timer");
+
+    $('.menu__btn').click(function(){
+    	$('.menu ul').slideToggle();
+    });	
+});
+
+// let fragment = document.createDocumentFragment();
+// const time = document.querySelector('#timerdd79587a0c12b5aee748d795eabe2e86');
+// fragment.appendChild(time);
+// const headerBg = document.querySelector('.header__bg');
+// headerBg.firstElementChild.appendChild(fragment);
